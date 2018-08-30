1c02ef1236e58efaf5951ce29f3704fac0a48392
Jan Czizikow <jan.czizikow@gmail.com>
2018-08-30 19:39:12 +0200

    Add config mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "skatespots_#{Rails.env}"
 
+  # Mailer stuff
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
